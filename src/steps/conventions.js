393b09032f79ecfb5242600ffddba5b82543fdc7
Noel MACE <Noel.Mace@ext.saint-gobain.com>
2018-03-29 15:30:51 +0200

    refactor: split convention into functions

--- src/steps/conventions.js
+++ src/steps/conventions.js
@@ -2,39 +2,30 @@ const semver = require('semver')
 const {dirname, basename, join} = require('path')
 const {libDestFolder} = require('../constants.js')
 
-/**
- * Ensure that our conventions on npm libraries are respected
- * @param {string} pkg path to the library's package.json file
- * @param {string} cwd working directory
- * @param {boolean} [debug=false] complete information on error reasons (slower solution)
- */
-function ensureConventions(pkg, cwd, debug = false) {
-  const libName = basename(dirname(pkg))
-  const errorMessage = (msg) => `${pkg} does not follow the conventions.
-${msg}
-${libName} build process has been aborted.`
-
-  const packageJson = require(join(cwd, pkg))
-
-  // Ensure that the destination folder is a "libName" subfolder of the configured "libDestFolder"
+// Ensure that the destination folder is a "libName" subfolder of the configured "libDestFolder"
+function buildDest({packageJson, libName, errorMessage}) {
   const dest = packageJson.ngPackage.dest
   const conventionDest = `${libDestFolder}/${libName}`
   if (!dest || dest !== conventionDest) {
     throw new Error(errorMessage(`${dest} should be ${conventionDest}`))
   }
+}
 
-  // the package.json file can not specify any devDependency
+// the package.json file can not specify any devDependency
+function devDependencies({packageJson, errorMessage}) {
   if (packageJson.devDependencies) {
     throw new Error(errorMessage(`You should not specify any devDependency in this file.`))
   }
+}
 
+// each lib's dependency should be satisfied by a global dependency
+function satisfiedDependencies({packageJson, errorMessage, cwd, debug}) {
   const getDependencies = (pkg) => ({
     ...pkg.dependencies,
     ...pkg.peerDependencies,
     ...pkg.optionalDependencies,
   })
 
-  // each lib's dependency should be satisfied by a global dependency
   const rootDependencies = getDependencies(require(join(cwd, 'package.json')))
   const dependencies = Object.entries(getDependencies(packageJson))
   let isVersionOk = true;
@@ -57,6 +48,28 @@ ${libName} build process has been aborted.`
   if (!isVersionOk) {
     throw new Error(errorMessage(`Every dependency should be consistent with the root package.json.`))
   }
+}
+
+/**
+ * Ensure that our conventions on npm libraries are respected
+ * throw an error if not
+ * @param {string} pkg path to the library's package.json file
+ * @param {string} cwd working directory
+ * @param {boolean} [debug=false] complete information on error reasons (slower solution)
+ */
+function ensureConventions(pkg, cwd, debug = false) {
+  const params = {
+    libName: basename(dirname(pkg)),
+    errorMessage: (msg) => `${pkg} does not follow the conventions.`
+      + `\n${msg}`
+      + `\n${basename(dirname(pkg))} build process has been aborted.`,
+    packageJson: require(join(cwd, pkg)),
+    pkg, cwd, debug
+  }
+
+  buildDest(params)
+  devDependencies(params)
+  satisfiedDependencies(params)
 }
 
 module.exports = {
